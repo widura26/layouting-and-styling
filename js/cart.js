69036331d3567bd35a7af4bb555684d82f21c1e7
widura26 <widurategalrejo@gmail.com>
2023-07-08 11:18:24 +0700

    eleventh commit

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,10 @@
 
 const gridCard = document.querySelector('.grid-card');
-
-  async function getData(){
+const crd = document.querySelectorAll('.card');
+const totalItem = document.querySelector('.cart .total-item');
+const totalPrice = document.querySelector('.cart .total-price');
+  
+  async function getData() {
     const response = await axios.get('https://dummyjson.com/products');
     const data = response.data.products;
     displayData(data);
@@ -21,28 +24,60 @@ const gridCard = document.querySelector('.grid-card');
 
 
 function card(title, description, price, stock, thumbnail){
+  
   return `
     <div class="card">
-    <div class="title-content-pricestock">
-    <div class="image">
-      <img src="${thumbnail}">
-    </div>
-        <div class="card-title">
-          <p>${title}</p>
+      <div class="title-content-pricestock">
+        <div class="image">
+          <img src="${thumbnail}">
         </div>
-        <div class="card-content">
-          <p>${description}</p>
-        </div>
-        <div class="card-price-stock">
-          <div class="card-price">
-            <p>${price}</p>
+          <div class="card-title">
+            <p>${title}</p>
           </div>
-          <div class=""card-stock>
-            <p>${stock}</p>
+          <div class="card-content">
+            <p>${description}</p>
           </div>
-        </div>
+          <div class="card-price-stock">
+            <div class="card-price">
+              <p>${price}</p>
+            </div>
+            <div class="card-stock">
+              <p>Stock : ${stock}</p>
+            </div>
+          </div>
       </div>
     </div>
   `
+}
+
+document.addEventListener('click', (e) => {
+  const target = e.target.closest('.card');  
+  if(target){
+    const price = parseInt(target.querySelector('.card-price p').textContent);
+    
+    if(target.classList.toggle('clicked')){
+      addition(price);
+    }else{
+      substraction(price);
+    }
+  }
+})
+
+let cart = [];
+let angka = 0;
+let totpri = 0;
+
+function addition(price){
+  totpri = totpri + price;
+  angka++;
+  totalItem.innerHTML = angka;
+  totalPrice.innerHTML = totpri; 
+}
+
+function substraction(price){
+  totpri = totpri - price;
+  angka--;
+  totalItem.innerHTML = angka;
+  totalPrice.innerHTML = totpri;
 }
 
